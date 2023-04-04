cf6531298227dad24aec5c8265a4da8cc983641c
hlainghlaing <44288265+hlainghlaing@users.noreply.github.com>
2023-04-04 15:11:01 +0630

    use regex patten to validate password

--- src/main/java/scm/com/ojt/tutorial6/Tutorial6.java
+++ src/main/java/scm/com/ojt/tutorial6/Tutorial6.java
@@ -1,16 +1,20 @@
 package scm.com.ojt.tutorial6;
 
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Tutorial6 {
+    private static final String PASSWORD_PATTERN = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#&()–[{}]:;',?/*~$^+=<>]).{8,20}$";
+    private static final Pattern pattern = Pattern.compile(PASSWORD_PATTERN);
 
     public static void main(String[] args) {
-        int passwordLength = 8;
-        boolean upChar = false;
-        boolean lowChar = false;
-        boolean special = false;
-        boolean digit = false;
-        char pwChar;
+//        int passwordLength = 8;
+//        boolean upChar = false;
+//        boolean lowChar = false;
+//        boolean special = false;
+//        boolean digit = false;
+//        char pwChar;
         // Ask user to enter password
         Scanner input = new Scanner(System.in);
         System.out.println("*Password must be at least 8 numbers.*");
@@ -18,25 +22,22 @@ public class Tutorial6 {
         System.out.print("Enter passwrord : ");
         String userInput = input.nextLine();
         input.close();
-        if (userInput.length() < passwordLength) {
+        if (userInput.length() < 8) {
             System.out.println("Invalid Password! Enter password at least 8 numbers.");
         } else {
-            for (int i = 0; i < userInput.length(); i++) {
-                // test each character of user input
-                pwChar = userInput.charAt(i);
-                // Call static method from Character Class
-                if (Character.isUpperCase(pwChar)) {
-                    upChar = true;
-                } else if (Character.isLowerCase(pwChar)) {
-                    lowChar = true;
-                } else if (Character.isDigit(pwChar)) {
-                    digit = true;
-                } else {
-                    special = true;
-                }
-            }
-            System.out.println(
-                    (upChar && lowChar && digit && special) ? "Your password is Strong!" : "Your password is Weak!");
+            Matcher matcher = pattern.matcher(userInput);
+            System.out.println((matcher.matches()) ? "Your Password is strong!" : "Your Password is weak!");
         }
+        /**
+         * if (userInput.length() < passwordLength) { System.out.println("Invalid
+         * Password! Enter password at least 8 numbers."); } else { for (int i = 0; i <
+         * userInput.length(); i++) { // test each character of user input pwChar =
+         * userInput.charAt(i); // Call static method from Character Class if
+         * (Character.isUpperCase(pwChar)) { upChar = true; } else if
+         * (Character.isLowerCase(pwChar)) { lowChar = true; } else if
+         * (Character.isDigit(pwChar)) { digit = true; } else { special = true; } }
+         * System.out.println( (upChar && lowChar && digit && special) ? "Your password
+         * is Strong!" : "Your password is Weak!"); }
+         */
     }
 }
